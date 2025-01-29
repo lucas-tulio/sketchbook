4c9c7b116dbb6378acde460bef7272e176828da2
Lucas Tulio <lucasctulio@gmail.com>
2025-01-29 19:44:03 -0300

    feat: add Starfield controls

--- src/sketches/Starfield.js
+++ src/sketches/Starfield.js
@@ -1,7 +1,13 @@
 import { DEFAULT_WIDTH, DEFAULT_HEIGHT } from '@/sketches/constants'
 
 export function Starfield(p5, onLoad) {
-  let numNodes = 3000
+  const DEFAULT_NUM_NODES = 1200
+  const DEFAULT_SPEED = 2.5
+  const DEFAULT_TRAIL = 0.85
+
+  let trail
+  let numNodes
+  let speedModifier
   let nodes = []
 
   class Node {
@@ -18,8 +24,8 @@ export function Starfield(p5, onLoad) {
 
     update() {
       const ratio = p5.width / p5.height
-      this.speed.x = (this.x / p5.width - 0.5) * ratio * 5 * this.acceleration
-      this.speed.y = (this.y / p5.height - 0.5) * 5 * this.acceleration
+      this.speed.x = (this.x / p5.width - 0.5) * ratio * speedModifier.value() * this.acceleration
+      this.speed.y = (this.y / p5.height - 0.5) * speedModifier.value() * this.acceleration
       this.x += this.speed.x
       this.y += this.speed.y
       if (this.x >= p5.width || this.x <= 0 || this.y >= p5.height || this.y <= 0) {
@@ -49,9 +55,36 @@ export function Starfield(p5, onLoad) {
     p5.background(0)
     p5.colorMode(p5.HSB)
     p5.noStroke()
-    for (let i = 0; i < numNodes; i++) {
+
+    p5.createElement('span', 'Nodes').parent('controls')
+    numNodes = p5.createSlider(100, 5000, DEFAULT_NUM_NODES, 100).parent('controls')
+    p5.createElement('br').parent('controls')
+
+    p5.createElement('span', 'Speed').parent('controls')
+    speedModifier = p5.createSlider(1, 5, DEFAULT_SPEED, 0.1).parent('controls')
+    p5.createElement('br').parent('controls')
+
+    p5.createElement('span', 'Trail').parent('controls')
+    trail = p5.createSlider(0.01, 0.95, DEFAULT_TRAIL, 0.01).parent('controls')
+    p5.createElement('br').parent('controls')
+
+    for (let i = 0; i < numNodes.value(); i++) {
       nodes.push(new Node())
     }
+
+    numNodes.elt.addEventListener('input', (event) => {
+      const newNodeCount = event.target.value
+      let diff = nodes.length - newNodeCount
+      const addNode = diff < 0 ? true : false
+      const clampedDiff = diff > 0 ? diff : -diff
+      for (let i = 0; i < clampedDiff; i++) {
+        if (addNode) {
+          nodes.push(new Node())
+        } else {
+          nodes.pop()
+        }
+      }
+    })
 
     if (onLoad) {
       onLoad()
@@ -59,7 +92,7 @@ export function Starfield(p5, onLoad) {
   }
 
   p5.draw = () => {
-    p5.background(0, 0, 0, 0.05)
+    p5.background(0, 0, 0, 1 - trail.value())
     for (let node of nodes) {
       node.update()
       node.show()
